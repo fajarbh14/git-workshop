52e745430bcfce790afd02b6737add8a2ad389a4
Adinz <87767683+adindaregita@users.noreply.github.com>
2023-11-04 12:07:26 +0700

    ubah keranjang belanja jadi cart

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  port: 5000,
+  port: 5001,
   supabase: {
     SUPABASE_URL: 'https://ragezwxaourlhuzmrnbd.supabase.co',
     SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJhZ2V6d3hhb3VybGh1em1ybmJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTYxNjE5ODksImV4cCI6MTk3MTczNzk4OX0.yDQEPvh2d-XBr1DAn3Uv_Ql0FAg293yTrlW9x4Gn848'
